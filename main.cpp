f9896137b6ce17e7e07ef1a665f4d5c8b0f15439
thawan-xyz <thawan.xyz@gmail.com>
2024-05-26 14:00:27 -0300

    first update

--- main.cpp
+++ main.cpp
@@ -1,12 +1,47 @@
 #include <iostream>
+#include <list>
+#include <optional>
 using namespace std;
 
-template <typename E>
+template <typename T, typename E, int L>
 class SeparateChaining {
 public:
+    SeparateChaining() {
+        hashTableSize = L;
+        numElements = 0;
+        hashTable = new list<pair<T, E>>[L];
+    }
+
+    int hashFunction(T key) {
+        return key % hashTableSize;
+    }
+
+    void insert(T key, E value) {
+        if (!find(key).has_value()) {
+            int hashPosition = hashFunction(key);
+            hashTable[hashPosition].push_back(make_pair(key, value));
+        }
+    }
+
+    optional<E> find(T key) {
+        int hashPosition = hashFunction(key);
+        auto listIterator = hashTable[hashPosition].begin();
+        while (listIterator != hashTable[hashPosition].end()) {
+            if (*listIterator->first == key) {
+                return listIterator->second;
+            }
+            ++listIterator;
+        }
+        return nullopt;
+    }
 
 private:
+    int hashTableSize;
+    int numElements;
+    list<pair<T, E>>* hashTable;
 };
 int main() {
+    auto* myDictionay = new SeparateChaining<int, string, 14>;
+    delete myDictionay;
     return 0;
 }
